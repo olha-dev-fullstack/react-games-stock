fb570080913d03a82ae293da6164c4b5c0b90fd8
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-04-02 00:11:34 +0300

    add game details to the page

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,9 @@
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
+import ExpandableText from "../components/ExpandableText";
+import GameAttributes from "../components/GameAttributes";
 import { useGame } from "../hooks/useGame";
+
 const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
@@ -12,7 +15,8 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 };
