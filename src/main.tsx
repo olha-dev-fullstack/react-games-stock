42785f35d54db118e857fea67f3357b6038e5bd4
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-04-01 12:56:55 +0300

    add react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-import App from './App'
-import theme from './theme'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import App from "./App";
+import theme from "./theme";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+const queryCLient = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
-      <App />
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <QueryClientProvider client={queryCLient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,14 @@
 import axios from "axios";
+console.log(import.meta.env.VITE_RAWG_API_KEY);
 
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: import.meta.env.VITE_RAWG_API_KEY,
   },
 });
+
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
