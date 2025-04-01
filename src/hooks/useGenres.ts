91c222a573b9d28624f49e68ece465367896dd15
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-04-01 23:51:13 +0300

    refactor entities interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,20 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
 import genres from "../data/genres";
 import APIClient from "../services/api-client";
-import ms from 'ms';
+import { Genre } from "../entities/Genre";
 
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-}
 const apiClient = new APIClient<Genre>("/genres");
 
 const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: ms('24h'),
+    staleTime: ms("24h"),
     initialData: genres,
   });
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,13 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
 import platforms from "../data/platforms";
+import { Platform } from "../entities/Platform";
 import APIClient from "../services/api-client";
-import ms from 'ms';
-
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 
@@ -15,7 +10,7 @@ const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: ms('24h'),
+    staleTime: ms("24h"),
     initialData: platforms,
   });
 
